be4274a1ecfc19acdd180698092c2ae97e227e3d
BrunoBoehm <bruno.boehm@gmail.com>
2017-10-17 22:20:09 +0200

    added custom content code support for the kpi banner

--- custom-content.php
+++ custom-content.php
@@ -131,6 +131,26 @@ function cush_custom_page() {
 		}
 	};
 
+	if ( get_field('kpi_banner', 'option') ) {
+		// var_dump(get_field('kpi_banner', 'option'));
+		$kpi_banner = get_field('kpi_banner', 'option');
+		echo '<section class="kpi-banner">';
+		echo 	'<h3>' . $kpi_banner['title'] . '</h3>';
+		echo 	'<div class="one-third first">';
+		echo 		'<span class="kpi-count">' . $kpi_banner['members_count'] . '</span>';
+		echo 		'<p class="kpi-text">' . $kpi_banner['members_text'] . '</p>';
+		echo 	'</div>';
+		echo 	'<div class="one-third">';
+		echo 		'<span class="kpi-count">' . $kpi_banner['actions_count'] . '</span>';
+		echo 		'<p class="kpi-text">' . $kpi_banner['actions_text'] . '</p>';
+		echo 	'</div>';
+		echo 	'<div class="one-third">';
+		echo 		'<span class="kpi-count">' . $kpi_banner['stories_count'] . '</span>';
+		echo 		'<p class="kpi-text">' . $kpi_banner['stories_text'] . '</p>';
+		echo 	'</div>';				
+		echo '</section>';
+	}
+
 }
 
 // Build the page
