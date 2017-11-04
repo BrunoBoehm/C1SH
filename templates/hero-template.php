1fe091a6c58cd1f8f944b881487a12143f3faf52
BrunoBoehm <bruno.boehm@gmail.com>
2017-11-04 16:33:45 +0100

    improved hero responsive layout and overlay

--- templates/hero-template.php
+++ templates/hero-template.php
@@ -8,41 +8,52 @@
 if( have_rows('hero_slider') ) {
     while( have_rows('hero_slider') ) {
         the_row();
-        $related_institutions = get_field('related_institution');
-        $related_actions = get_field('related_action');
-        $institution_logo = get_field('institution_logo');
-        // print_r($related_institution);
 
-        echo '<div class="hero-section overlay">';
+        if ( is_page() &&  ! get_sub_field('title') ) {
+            echo '<div class="hero-section">';
+        } else {
+            echo '<div class="hero-section overlay">';
+        }
         echo 	'<div class="wp-custom-header">';
         echo		'<img src="' . get_sub_field('image')['url'] . '">';			
         echo 	'</div>';
 
         echo 	'<div class="wrap">';
 
-        // In case there is/are related institution, show logo(s) 
-        if ( $related_institutions ) {
-            echo    '<div class="brand-logos">';
-            foreach ( $related_institutions as $institution ) {
-            echo        '<div class="brand-logo"><a href="' . get_permalink( $institution->ID ) . '" title="' . $institution->post_title . '"><img src="' . get_field('institution_logo', $institution->ID )['url'] . '"></a></div>';
-            };
-            echo    '</div>';
-        } elseif ( $institution_logo ) {
-            echo    '<div class="brand-logos">';
-            echo        '<div class="brand-logo"><img src="' . $institution_logo['url'] . '"></div>';
-            echo    '</div>';
-        };
+        if ( ! is_page() ) {
+            $related_institutions = get_field('related_institution');
+            $related_actions = get_field('related_action');
+            $institution_logo = get_field('institution_logo');
+            // print_r($related_institution);
 
-        echo		'<h1>' . get_sub_field('title') . '</h1>';
-
-        // In case there are related actions, list them
-        if ( $related_actions ) {
-            echo    '<div class="related-actions">';
-            foreach ( $related_actions as $action ) {
-            echo        '<a href="' . get_permalink( $action->ID ) . '">' . $action->post_title . '</a>';
+            // In case there is/are related institution, show logo(s) 
+            if ( $related_institutions ) {
+                echo    '<div class="brand-logos">';
+                foreach ( $related_institutions as $institution ) {
+                echo        '<div class="brand-logo"><a href="' . get_permalink( $institution->ID ) . '" title="' . $institution->post_title . '"><img src="' . get_field('institution_logo', $institution->ID )['url'] . '"></a></div>';
+                };
+                echo    '</div>';
+            } elseif ( $institution_logo ) {
+                echo    '<div class="brand-logos">';
+                echo        '<div class="brand-logo"><img src="' . $institution_logo['url'] . '"></div>';
+                echo    '</div>';
             };
-            echo    '</div>';
-        };
+        }    
+
+        if ( get_sub_field('title') ) {
+            echo		'<h1>' . get_sub_field('title') . '</h1>';
+        }
+
+        if ( ! is_page() ) {    
+            // In case there are related actions, list them
+            if ( $related_actions ) {
+                echo    '<div class="related-actions">';
+                foreach ( $related_actions as $action ) {
+                echo        '<a href="' . get_permalink( $action->ID ) . '">' . $action->post_title . '</a>';
+                };
+                echo    '</div>';
+            };
+        }
 
         // Optional CTA button
         if ( get_sub_field('link') ) {
