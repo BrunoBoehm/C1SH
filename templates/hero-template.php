ad4eae7bf921bc1cedac9c70cae75b44acf0e831
BrunoBoehm <bruno.boehm@gmail.com>
2017-11-04 15:09:26 +0100

    added categories inline on hero

--- templates/hero-template.php
+++ templates/hero-template.php
@@ -17,7 +17,10 @@ if( have_rows('hero_slider') ) {
         echo 	'<div class="wp-custom-header">';
         echo		'<img src="' . get_sub_field('image')['url'] . '">';			
         echo 	'</div>';
+
         echo 	'<div class="wrap">';
+
+        // In case there is/are related institution, show logo(s) 
         if ( $related_institutions ) {
             echo    '<div class="brand-logos">';
             foreach ( $related_institutions as $institution ) {
@@ -29,7 +32,10 @@ if( have_rows('hero_slider') ) {
             echo        '<div class="brand-logo"><img src="' . $institution_logo['url'] . '"></div>';
             echo    '</div>';
         };
+
         echo		'<h1>' . get_sub_field('title') . '</h1>';
+
+        // In case there are related actions, list them
         if ( $related_actions ) {
             echo    '<div class="related-actions">';
             foreach ( $related_actions as $action ) {
@@ -37,11 +43,24 @@ if( have_rows('hero_slider') ) {
             };
             echo    '</div>';
         };
+
+        // Optional CTA button
         if ( get_sub_field('link') ) {
             echo	'<a href="' . get_sub_field('link')['url'] . '" class="button">' . get_sub_field('link')['title'] . '</a>';				
         }
-        echo 	'</div>';
-        echo '</div>';
+
+        // If Contagion post type, list categories
+        if ( get_post_type( get_the_ID() ) == 'contagion' ) {
+            $categories = get_the_category();
+            echo    '<div class="related-categories">';
+            foreach ($categories as $category) {
+            echo        '<a href="' . get_category_link( $category->term_id ) . '" title="' . __( "Principe d'action", 'C1SH' ) . ' : ' . $category->name . '">' . $category->name . '</a>';
+            }
+            echo    '</div>';
+        }
+
+        echo 	'</div>'; // end of wrap
+        echo '</div>'; // end of hero section
         // print_r(get_sub_field('btn_text'));
     } 
 };
